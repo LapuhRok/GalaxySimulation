bfdb3caffb8c8837a742375e2b17621cc4a92e99
Rok Lapuh <Ajrok@MacBook-Pro.local>
2016-05-10 20:22:01 +0200

    gals created
    Program bounce changed to simple galsim, that measures time and works
    with basic force update.

--- file_operations.c
+++ file_operations.c
@@ -5,7 +5,7 @@ int read_doubles_from_file(int N, double* p, const char* fileName) {
   // Open input file and determine its size
   FILE* input_file = fopen(fileName, "rb");
   if(!input_file) {
-    printf("read_doubles_from_file error: failed to open input file '%s'.\n", fileName);
+    printf("read_doubles_from_file error: failed to open input file '%s'.\n", fileName); fflush(stdout);
     return -1;
   }
   // Get filesize using fseek() and ftell().
@@ -16,19 +16,19 @@ int read_doubles_from_file(int N, double* p, const char* fileName) {
   if(fileSize != N * sizeof(double)) {
     printf("read_doubles_from_file error: size of input file '%s' does not match the given N.\n", fileName);
     printf("For N = %d the file size is expected to be (N * sizeof(double)) = %lu but the actual file size is %lu.\n",
-	   N, N * sizeof(double), fileSize);
+	   N, N * sizeof(double), fileSize); fflush(stdout);
     return -1;
   }
   // Read contents of input_file into buffer
   size_t noOfItemsRead = fread(p, sizeof(char), fileSize, input_file);
   if(noOfItemsRead != fileSize) {
-    printf("read_doubles_from_file error: failed to read file contents into buffer.\n");
+    printf("read_doubles_from_file error: failed to read file contents into buffer.\n"); fflush(stdout);
     return -1;
   }
   // OK, now we have the file contents in the buffer.
   // Since we are finished with the input file, we can close it now.
   if(fclose(input_file) != 0) {
-    printf("read_doubles_from_file error: error closing input file.\n");
+    printf("read_doubles_from_file error: error closing input file.\n"); fflush(stdout);
     return -1;
   }
   // Return 0 to indicate success
@@ -38,18 +38,18 @@ int read_doubles_from_file(int N, double* p, const char* fileName) {
 int write_doubles_to_file(int N, const double* p, const char* fileName) {
   FILE* output_file = fopen(fileName, "wb");
   if(!output_file) {
-    printf("write_doubles_to_file error: failed to open output file '%s'.\n", fileName);
+    printf("write_doubles_to_file error: failed to open output file '%s'.\n", fileName); fflush(stdout);
     return -1;
   }
   // Now write to the file using fwrite()
   size_t noOfItemsWritten = fwrite(p, sizeof(double), N, output_file);
   if(noOfItemsWritten != N) {
-    printf("write_doubles_to_file error: failed to write buffer contents to output file.\n");
+    printf("write_doubles_to_file error: failed to write buffer contents to output file.\n"); fflush(stdout);
     return -1;
   }
   // OK, we have successfully written to the output file. Now close it.
   if(fclose(output_file) != 0) {
-    printf("write_doubles_to_file error: error closing output file.\n");
+    printf("write_doubles_to_file error: error closing output file.\n"); fflush(stdout);
     return -1;
   }
   // Return 0 to indicate success
--- galsim.c
+++ galsim.c
@@ -1,109 +1,200 @@
-/*
- * File: bounce.c
- * --------------
- * An example code to demonstrate graphics routines
- *
- */
+// galsim.c
+// High Perfomrance Computing - Assignment
+// Rok Lapuh
 
 #include "rand.h"
 #include "graphics.h"
+#include "file_operations.c"
 #include <GLUT/glut.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
 // For Linux, you may need GL/glut.h instead:
 //#include <GL/glut.h>
-#include <math.h>
 
+// Declarations of functions
 void Bounce(double *x, double *y, double *u, double *v);
+void updateForce(int i);
 
+// Constant parameters
+int N;
+int nsteps;
+double delta_t;
+double theta_max;
+double G;
+double e0 = 1e-3;
+
+// Iterations counter
+int iter = 0;
+
+// Data
+double *p;
 double *x,*y,*mass,*forceX,*forceY,*u,*v;
-float dt=1e-3,grav=10;
 
-int N = 10;
+// Timers
+double totalRunTime = 0;    // time of running the program
+double totalCodeTime = 0;   // time of running my code
+double startRunTime;
+double startCodeTime;
+double endTime;
+
+static double get_wall_seconds() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    double seconds = tv.tv_sec + (double)tv.tv_usec / 1000000;
+    return seconds;
+}
 
 // This function is called every time GLUT refreshes the display.
 void display(void)
 {
-    int i;
-    double ax, ay;
-    for(i=0;i<N;i++) {
-      ax=forceX[i]/mass[i];
-      ay=forceY[i]/mass[i]-grav;
-
-      u[i]+=ax*dt;
-      v[i]+=ay*dt;
-      x[i]+=u[i]*dt;
-      y[i]+=v[i]*dt;
-
-      Bounce(&x[i],&y[i],&u[i],&v[i]);
+    startCodeTime = get_wall_seconds();
+    
+    // Update force
+    for(int i = 0; i < N; i++) {
+        updateForce(i);
     }
     
-    drawPoints(x,y,N);
-       	    	
-}
-
-
-
-int main(int argc, char *argv[]) {    
-   
-  // Initialize the graphics routines
-  graphicsInit(&argc, argv, display);
+    // Update positions
+    double ax, ay;
+    for(int i = 0; i < N; i++) {
+        
+        ax=forceX[i]/mass[i];
+        ay=forceY[i]/mass[i];
+        
+        u[i] += ax*delta_t;
+        v[i] += ay*delta_t;
+        x[i] += u[i]*delta_t;
+        y[i] += v[i]*delta_t;
+        
+        // Uncomment to force points to stay in bounds
+        // Bounce(&x[i],&y[i],&u[i],&v[i]);
+    }
     
-
-  int i;
-
-  x = (double *)malloc(N*sizeof(double));
-  y = (double *)malloc(N*sizeof(double));
-  u = (double *)malloc(N*sizeof(double));
-  v = (double *)malloc(N*sizeof(double));
-  mass = (double *)malloc(N*sizeof(double));
-  forceX = (double *)malloc(N*sizeof(double));
-  forceY = (double *)malloc(N*sizeof(double));
-
-
-  for(i=0;i<N;i++) {
-    x[i]=frand(-1,1);
-    y[i]=frand(-.75,.75);
-    u[i]=frand(-1,1);
-    v[i]=frand(-1,1);
-    mass[i]=1;
-    forceX[i]=0;
-    forceY[i]=0;
-  }
-  
-  glutMainLoop();
-	
-  return 0;
-}
-
-/*
- * Function: Bounce
- * Usage: Bounce(&x[i],&y[i],&u[i],&v[i]);
- * -------------------------------------------
- * If a particle moves beyond any of the boundaries then bounce it back
- *
- */
-void Bounce(double *x, double *y, double *u, double *v) {
-  double W = 1.0f, H = 1.0f;
-  if(*x>W) {
-    *x=2*W-*x;
-    *u=-*u;
-  }
-
-  if(*x<0) {
-    *x=-*x;
-    *u=-*u;
-  }
-
-  if(*y>H) {
-    *y=2*H-*y;
-    *v=-*v;
-  }
-
-  if(*y<0) {
-    *y=-*y;
-    *v=-*v;
-  }
+    // Write to file
+    iter++;
+    if (iter == nsteps) {
+        printf("Done!\n");
+        endTime = get_wall_seconds();
+        totalRunTime = startRunTime - endTime;
+        totalCodeTime += startCodeTime - endTime;
+        printf("Total running time: %f\n", totalRunTime);
+        printf("Running time: %f\n", totalCodeTime);
+    }
+    
+    totalCodeTime += startCodeTime - get_wall_seconds();
+    
+    // Display
+    if (iter % 100 == 0) {
+        drawPoints(x,y,N);
+    }
+    
 }
 
 
-  
-  
+
+int main(int argc, char *argv[]) {
+    
+    startRunTime = get_wall_seconds();
+    
+    // Check input parameters
+    if (argc != 6) {
+        printf("Input Error!\nRun: $ ./galsim N filename nsteps delta_t theta_max\n");
+        return -1;
+    }
+    
+    // Read input parameters
+    sscanf(argv[1],"%d",&N);
+    sscanf(argv[3],"%d",&nsteps);
+    sscanf(argv[4],"%lf",&delta_t);
+    sscanf(argv[5],"%lf",&theta_max);
+    
+    // Calculate gravitational constant
+    G = 100/N;
+    
+    // Initialize the graphics routines
+    graphicsInit(&argc, argv, display);
+    
+    // Allocate memory for variables
+    p = (double *)malloc(5*N*sizeof(double));
+    x = (double *)malloc(N*sizeof(double));
+    y = (double *)malloc(N*sizeof(double));
+    u = (double *)malloc(N*sizeof(double));
+    v = (double *)malloc(N*sizeof(double));
+    mass = (double *)malloc(N*sizeof(double));
+    forceX = (double *)malloc(N*sizeof(double));
+    forceY = (double *)malloc(N*sizeof(double));
+    
+    // Read input file
+    int flag = read_doubles_from_file(5*N, p, argv[2]);
+    if (flag != 0){
+        printf("Error reading data from file.\n"); fflush(stdout);
+        return -1;
+    }
+    
+    // Separate initial conditions
+    for (int i = 0; i < N; i++) {
+        mass[i] = p[5*i + 0];
+        x[i] = p[5*i + 1];
+        y[i] = p[5*i + 2];
+        u[i] = p[5*i + 3];
+        v[i] = p[5*i + 4];
+    }
+    
+    // Run
+    glutMainLoop();
+    
+    return 0;
+}
+
+void Bounce(double *x, double *y, double *u, double *v)
+{
+    double W = 1.0f, H = 1.0f;
+    if(*x>W) {
+        *x=2*W-*x;
+        *u=-*u;
+    }
+    
+    if(*x<0) {
+        *x=-*x;
+        *u=-*u;
+    }
+    
+    if(*y>H) {
+        *y=2*H-*y;
+        *v=-*v;
+    }
+    
+    if(*y<0) {
+        *y=-*y;
+        *v=-*v;
+    }
+}
+
+void updateForce(int i)
+{
+    double sumX = 0;
+    double sumY = 0;
+    double xi = x[i];
+    double yi = y[i];
+    double xj, yj, rx, ry, r, rr;
+    for (int j = 0; j < N; j++) {
+        if (i == j) continue;
+        xj = x[j];
+        yj = y[j];
+        rx = xi - xj;
+        ry = yi - yj;
+        r = sqrt(rx*rx + ry*ry);
+        rr = r + e0;
+        sumX += mass[j]*rx/(rr*rr*rr);
+        sumY += mass[j]*ry/(rr*rr*rr);
+    }
+    forceX[i] = -G*mass[i]*sumX;
+    forceY[i] = -G*mass[i]*sumY;
+}
+
+
+
+
