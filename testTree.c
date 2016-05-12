30f5673ccca5d8b3ed5a88a46f238cf97e51f02f
Rok Lapuh <Ajrok@MacBook-Pro.local>
2016-05-12 21:21:32 +0200

    Working testTree
    testTree now correctly creates a quad tree. It still needs to be
    populated with values.

--- testTree.c
+++ testTree.c
@@ -1,40 +1,9 @@
-#include <GLUT/glut.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/time.h>
-
-// Tree structure
-struct treeNode_ {
-    double centerX;
-    double centerY;
-    double nodeMass;
-    int numberOfParticles;
-    double leftBorder;
-    double rightBorder;
-    double upBorder;
-    double downBorder;
-    struct treeNode_* NW;
-    struct treeNode_* NE;
-    struct treeNode_* SW;
-    struct treeNode_* SE;
-    
-};
-typedef struct treeNode_ treeNode;
-
-// Declarations
-void createTree(treeNode* rootNode);
-void insertInTree(int i, treeNode* node);
-void free_tree(treeNode* rootNode);
-void print_tree(treeNode* rootNode, int level);
-double *x,*y,*mass,*forceX,*forceY,*u,*v;
-int N;
+#include "testTree.h"
 
 
 int main(int argc, char *argv[]) {
     
-    N = 1;
+    N = 3;
     
     // Allocate memory for variables
     x = (double *)malloc(N*sizeof(double));
@@ -44,16 +13,22 @@ int main(int argc, char *argv[]) {
     x[0] = 0.75;
     y[0] = 0.75;
     mass[0] = 1.0;
+    x[1] = 0.25;
+    y[1] = 0.25;
+    mass[1] = 1.0;
+    x[2] = 0.3;
+    y[2] = 0.3;
+    mass[2] = 2.0;
     
-    treeNode* rootNode = (treeNode*)malloc(sizeof(treeNode));
-    createTree(rootNode);
+    rootNode = (treeNode*)malloc(sizeof(treeNode));
+    createTree();
     print_tree(rootNode, 0);
     free_tree(rootNode);
     
     return 0;
 }
 
-void createTree(treeNode* rootNode)
+void createTree()
 {
     rootNode -> numberOfParticles = 0;
     rootNode -> downBorder = 0.0;
@@ -68,50 +43,144 @@ void createTree(treeNode* rootNode)
     for (int i = 0; i < N; i++) {
         insertInTree(i, rootNode);
     }
-    
-    
 }
 
 void insertInTree(int i, treeNode* node)
 {
-    node -> numberOfParticles++;
-    if (node -> numberOfParticles == 1) {
-        node -> centerX = x[i];
-        node -> centerY = y[i];
-        node -> nodeMass = mass[i];
+    if (node == rootNode) rootNode -> numberOfParticles++;
+    
+    // create children if they do not exist
+    if (!node -> SW) {
+        double midX = (node->leftBorder + node->rightBorder)/2;
+        double midY = (node->downBorder + node->upBorder)/2;
+        // NW
+        node -> NW = (treeNode*)malloc(sizeof(treeNode));
+        node -> NW -> numberOfParticles = 0;
+        node -> NW -> downBorder = midY;
+        node -> NW -> upBorder = node->upBorder;
+        node -> NW -> leftBorder = node->leftBorder;
+        node -> NW -> rightBorder = midX;
+        node -> NW -> NW = NULL;
+        node -> NW -> NE = NULL;
+        node -> NW -> SW = NULL;
+        node -> NW -> SE = NULL;
+        // SW
+        node -> SW = (treeNode*)malloc(sizeof(treeNode));
+        node -> SW -> numberOfParticles = 0;
+        node -> SW -> downBorder = node->downBorder;
+        node -> SW -> upBorder = midY;
+        node -> SW -> leftBorder = node->leftBorder;
+        node -> SW -> rightBorder = midX;
+        node -> SW -> NW = NULL;
+        node -> SW -> NE = NULL;
+        node -> SW -> SW = NULL;
+        node -> SW -> SE = NULL;
+        // NE
+        node -> NE = (treeNode*)malloc(sizeof(treeNode));
+        node -> NE -> numberOfParticles = 0;
+        node -> NE -> downBorder = midY;
+        node -> NE -> upBorder = node->upBorder;
+        node -> NE -> leftBorder = midX;
+        node -> NE -> rightBorder = node->rightBorder;
+        node -> NE -> NW = NULL;
+        node -> NE -> NE = NULL;
+        node -> NE -> SW = NULL;
+        node -> NE -> SE = NULL;
+        // SE
+        node -> SE = (treeNode*)malloc(sizeof(treeNode));
+        node -> SE-> numberOfParticles = 0;
+        node -> SE -> downBorder = node->downBorder;
+        node -> SE -> upBorder = midY;
+        node -> SE -> leftBorder = midX;
+        node -> SE -> rightBorder = node->rightBorder;
+        node -> SE -> NW = NULL;
+        node -> SE -> NE = NULL;
+        node -> SE -> SW = NULL;
+        node -> SE -> SE = NULL;
     }
+    
+    // Particle on the WEST
+    if (x[i] < (node->leftBorder + node->rightBorder)/2) {
+        // Particle on the SOUTH
+        if (y[i] < (node->downBorder + node->upBorder)/2) {
+            if (node -> SW -> numberOfParticles == 0) {
+                node -> SW -> id = i;
+                node -> SW -> numberOfParticles++;
+            } else if (node -> SW -> numberOfParticles == 1) {
+                node -> SW -> numberOfParticles++;
+                insertInTree(i, node->SW);
+                insertInTree(node->SW->id, node->SW);
+            } else {
+                insertInTree(i, node->SW);
+            }
+        // Particle on the NORTH
+        } else {
+            if (node -> NW -> numberOfParticles == 0) {
+                node -> NW -> id = i;
+                node -> NW -> numberOfParticles++;
+            } else if (node -> NW -> numberOfParticles == 1) {
+                node -> NW -> numberOfParticles++;
+                insertInTree(i, node->NW);
+                insertInTree(node->NW->id, node->NW);
+            } else {
+                insertInTree(i, node->NW);
+            }
+        }
+    }
+    // Particle on the EAST
     else {
-        printf("ELSE!\n");
+        // Particle on the SOUTH
+        if (y[i] < (node->downBorder + node->upBorder)/2) {
+            if (node -> SE -> numberOfParticles == 0) {
+                node -> SE -> id = i;
+                node -> SE -> numberOfParticles++;
+            } else if (node -> SE -> numberOfParticles == 1) {
+                node -> SE -> numberOfParticles++;
+                insertInTree(i, node->SE);
+                insertInTree(node->SE->id, node->SE);
+            } else {
+                insertInTree(i, node->SE);
+            }
+        }
+        // Particle on the NORTH
+        else {
+            if (node -> NE -> numberOfParticles == 0) {
+                node -> NE -> id = i;
+                node -> NE -> numberOfParticles++;
+            } else if (node -> NE -> numberOfParticles == 1) {
+                node -> NE -> numberOfParticles++;
+                insertInTree(i, node->NE);
+                insertInTree(node->NE->id, node->NE);
+            } else {
+                insertInTree(i, node->NE);
+            }
+        }
     }
 }
 
-void free_tree(treeNode* rootNode) {
-    if(rootNode->NW) free_tree(rootNode->NW);
-    if(rootNode->NE) free_tree(rootNode->NE);
-    if(rootNode->SW) free_tree(rootNode->SW);
-    if(rootNode->SE) free_tree(rootNode->SE);
-    free(rootNode);
+void free_tree(treeNode* node) {
+    if(node->NW) free_tree(node->NW);
+    if(node->NE) free_tree(node->NE);
+    if(node->SW) free_tree(node->SW);
+    if(node->SE) free_tree(node->SE);
+    free(node);
 }
 
-void print_tree(treeNode* rootNode, int level) {
-    int i;
-    for(i = 0; i < level; i++) printf("   ");
-    printf("x=%f y=%f m=%f\n", rootNode->centerX, rootNode->centerY, rootNode->nodeMass);
-    if(rootNode->NW){
-        printf("Subprinting!"); fflush(stdout);
-        print_tree(rootNode->NW, level+1);
+void print_tree(treeNode* node, int level) {
+    for(int i = 0; i < level; i++) printf("   ");
+    printf("x=%f y=%f m=%f id=%d #=%d\n", node->centerX, node->centerY, node->nodeMass, node->id, node->numberOfParticles);
+    
+    if(node->NW){
+        print_tree(node->NW, level+1);
     }
-    if(rootNode->NE){
-        printf("Subprinting!");
-        print_tree(rootNode->NE, level+1);
+    if(node->NE){
+        print_tree(node->NE, level+1);
     }
-    if(rootNode->SW){
-        printf("Subprinting!");
-        print_tree(rootNode->NW, level+1);
+    if(node->SW){
+        print_tree(node->SW, level+1);
     }
-    if(rootNode->SE){
-        printf("Subprinting!");
-        print_tree(rootNode->NE, level+1);
+    if(node->SE){
+        print_tree(node->SE, level+1);
     }
 }
 
---
+++ testTree.h
@@ -0,0 +1,41 @@
+#ifndef testTree_h
+#define testTree_h
+
+#include <GLUT/glut.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+// Tree structure
+struct treeNode_ {
+    double centerX;
+    double centerY;
+    double nodeMass;
+    int numberOfParticles;
+    double leftBorder;
+    double rightBorder;
+    double upBorder;
+    double downBorder;
+    struct treeNode_* NW;
+    struct treeNode_* NE;
+    struct treeNode_* SW;
+    struct treeNode_* SE;
+    int id;
+};
+typedef struct treeNode_ treeNode;
+
+
+// Declarations
+void createTree();
+void insertInTree(int i, treeNode* node);
+void free_tree(treeNode* node);
+void print_tree(treeNode* node, int level);
+double *x,*y,*mass,*forceX,*forceY,*u,*v;
+int N;
+treeNode* rootNode;
+
+
+
+#endif
